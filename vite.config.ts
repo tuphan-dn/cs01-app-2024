e92c8466269e3e297d34b07d7b25744b9f47ee7f
tuphan-dn <tuphan@descartes.network>
2024-11-29 15:55:24 +0700

    feat: static build

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { vitePlugin as remix } from '@remix-run/dev'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-declare module "@remix-run/node" {
+declare module '@remix-run/node' {
   interface Future {
-    v3_singleFetch: true;
+    v3_singleFetch: true
   }
 }
 
 export default defineConfig({
   plugins: [
     remix({
+      ssr: false,
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
@@ -21,4 +22,11 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
-});
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
+})
